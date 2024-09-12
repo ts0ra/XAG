245db3142d485f6897e09b86983aac7c27ece63a
ts0ra <gaming.heroes354@gmail.com>
2024-09-12 17:42:04 +0700

    [feat] - add auto enter token from api_token.txt

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from src.xag import XAG
+from pathlib import Path
 
 def main():
     print("Welcome to XAG APP by ts0ra!")
+
+    folder_path = Path('./data')
+    file_path = folder_path / 'api_token.txt'
+    folder_path.mkdir(parents=True, exist_ok=True)
+    if not file_path.exists():
+        file_path.touch()
+
+    with open(file_path, 'r') as f:
+        api_token = f.read().strip()
+        if api_token:
+            xag = XAG(api_token)
+            xag.run()
+            return
+
     api_token = str(input("Enter your API token: "))
     xag = XAG(api_token)
     xag.run()
